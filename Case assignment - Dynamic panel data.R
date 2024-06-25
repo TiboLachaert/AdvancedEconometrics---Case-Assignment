1d5527c1ae0375b2fe003b2f9a6397ab3cb0197a
Tibo Lachaert <tibolachaert@scarlet.be>
2024-06-25 21:31:03 +0200

    Added size in results and clean up

--- Case assignment - Dynamic panel data.R
+++ Case assignment - Dynamic panel data.R
@@ -1,14 +1,15 @@
-#########################################
-##     Take home exam 2024 - Part 1    ## 
-## Tibo Lachaert & Nicolas Romero Diaz ##
-#########################################
+#=====================================#
+#     Take home exam 2024 - Part 1    # 
+# Tibo Lachaert & Nicolas Romero Diaz #
+#=====================================#
 
-## Import functions and packages ----
+# Import functions and packages ----
 source("OwnFunctions.R")
 library(tidyverse)
 library(readr)
 library(fastDummies)
 library(matlib)
+library(openxlsx)
 
 # Case 0: OLS estimation -----
 
@@ -66,75 +67,10 @@ df_FE_X <- data_FE %>%
 FE <- FE_own(df_FE_y, df_FE_X)
 show(FE[1:4, ]) #Correction term added in function to match t-stats of assignment
 
+FE <- FE_own(df_FE_y, df_FE_X, correction = TRUE)
+show(FE[1:4, ]) #Correction term added in function to match t-stats of assignment
+
 ## Simulating properties of the FE estimator ----
-
-## Simulation methods for bias correction and inference ----
-
-#Define parameter grid
-burn_t <- 25
-rho_par <- c(0, 0.5, 0.9)
-T_par = c(4, 10, 20, 50)
-N_par = c(10, 100)
-
-#Generate samples
-generate_sample <- function(rho, N, T){ #Should be implemented with vectors
-  yit <- matrix(data = 0, nrow = T+1, ncol = N)
-  for (i in 1:N){
-    y <- 0
-      for (s in 1:burn_t){
-        y <- rho*y + rnorm(1)
-      }
-    yit[1, i] <- y
-    for (t in 1:T){
-      y <- rho*y + rnorm(1)
-      yit[t+1, i] <- y
-    }
-  }
-  return(yit)
-}
-
-#Perform Monte Carlo
-
-set.seed(11132)
-
-#ese = estimated standard error
-#tse = true standard error
-N_sim <- 10 #Number of simulations
-result_MC <- data.frame(rho = double(), T = integer(), N = integer(), 
-                        result_rho = double(), result_ese = double(), 
-                        result_tse = double(), result_an = double())
-
-for (rho in rho_par) {
-  for (T in T_par) {
-     for (N in N_par) {
-       print(c(rho, T, N))
-       rho_sim  <- c()
-       se_sim   <- c()
-       for (i in 1:N_sim) {
-        df_MC   <- generate_sample(rho, N, T)
-        df_MC_y <- df_MC[-1, ]
-        df_MC_x <- array(df_MC[-(T+1), ], dim=c(T, N, 1))
-        
-        FE_MC   <- FE_own(df_MC_y, df_MC_x)
-        rho_sim <- c(rho_sim, FE_MC[1])
-        se_sim  <- c(se_sim, FE_MC[2])
-       }
-       result_MC <- rbind(result_MC, list(rho = rho, T = T, N = N, 
-                                          result_rho = mean(rho_sim), result_ese = mean(se_sim), 
-                                          result_tse = sd(rho_sim), result_an = 1/sqrt(T * (1 - rho^2))))
-    }
-  }
-}
-
-result_MC$asymptotic_assumption <- result_MC$N / result_MC$T^3 
-result_MC$nickel_bias           <- - (1+result_MC$rho)/result_MC$T
-result_MC$nickel_bias_corrected <- result_MC$rho + result_MC$nickel_bias
-
-### Half-panel jackknife bias-corrected FE estimator ----
-
-### Bootstrap ----
-
-# Case 2: Generalized Method of Moments (GMM) estimation ----
 
 # Function to generate AR(1) panel data
 generate_ar1_panel <- function(rho, N, T) {
@@ -173,8 +109,72 @@ generate_ar1_panel <- function(rho, N, T) {
   return(panel_df)
 }
 
-## Test for one setting ----
+#Perform Monte Carlo
+N_sim <- 100 #Number of simulations
+rho_par <- c(0, 0.5, 0.9)
+T_par = c(4, 10, 20, 50)
+N_par = c(10, 100)
 
+set.seed(11132)
+result_FE_MC <- data.frame(rho = double(), T = integer(), N = integer(), 
+                        result_rho = double(), result_ese = double(), 
+                        result_tse = double(), result_an = double(), 
+                        size = double())
+#ese = estimated standard error
+#tse = true standard error
+
+for (rho in rho_par) {
+  for (T in T_par) {
+    for (N in N_par) {
+      print(c(rho, T, N))
+      rho_sim  <- c()
+      se_sim   <- c()
+      
+      for (i in 1:N_sim) {
+        df_MC   <- generate_ar1_panel(rho, N, T) %>%
+          group_by(individual) %>%
+          mutate(yit = value) %>%
+          mutate(yit_lag1 = lag(yit, n = 1)) %>%
+          na.omit()
+        
+        df_FE_x <- df_MC %>%
+          select(individual, time, yit_lag1)  %>%
+          pivot_longer(cols = -c(individual, time)) %>%
+          mutate(time = as.numeric(time) - 2) %>%
+          xtabs(data = ., value ~ time + individual + name)
+        
+        df_FE_y <- df_MC %>%
+          select(individual, time, yit) %>%
+          pivot_wider(names_from = individual, values_from = yit) %>%
+          select(-time) %>%
+          as.matrix()
+        
+        FE_MC   <- FE_own(df_FE_y, df_FE_x)
+        rho_sim <- c(rho_sim, FE_MC[1])
+        se_sim  <- c(se_sim, FE_MC[2])
+      }
+      result_FE_MC <- rbind(result_FE_MC, list(rho = rho, T = T, N = N, 
+                                         result_rho = mean(rho_sim), result_ese = mean(se_sim), 
+                                         result_tse = sd(rho_sim), result_an = 1/sqrt(T * (1 - rho^2)), 
+                                         size = sum(abs((rho_sim - rho)/se_sim) > 1.96) / N_sim))
+    }
+  }
+}
+
+result_FE_MC$asymptotic_assumption <- result_FE_MC$N / result_FE_MC$T^3 
+result_FE_MC$nickel_bias           <- - (1+result_FE_MC$rho)/result_FE_MC$T
+result_FE_MC$nickel_bias_corrected <- result_FE_MC$rho + result_FE_MC$nickel_bias
+
+## Simulation methods for bias correction and inference ----
+#See jackknife_bias_correction.R
+
+### Half-panel jackknife bias-corrected FE estimator ----
+
+### Bootstrap ----
+
+# Case 2: Generalized Method of Moments (GMM) estimation ----
+
+## Test for one setting ----
 rho <- 0.5
 N <- 10
 T <- 20
@@ -242,18 +242,13 @@ show(GMM)
 
 ## Monte Carlo ----
 
-Nsim <- 10 #Number of simulations
-rho_par <- c(0, 0.5, 0.9)
-T_par = c(4, 10, 20, 50)
-N_par = c(10, 100)
-
 ### With one instrument ----
 
-onestep_MC <- function(Nsim, rho, N, T){
-  results <- matrix(data = 0, nrow = Nsim, ncol = 5)
+onestep_MC <- function(N_sim, rho, N, T){
+  results <- matrix(data = 0, nrow = N_sim, ncol = 5)
   colnames(results) <- c("coefs", "stdvs", "tstats", "pvals", "Jstat")
   
-  for (i in 1:Nsim){
+  for (i in 1:N_sim){
     df_GMM <- generate_ar1_panel(rho, N, T) %>% 
       group_by(individual) %>%
       mutate(yit = value) %>%
@@ -290,35 +285,34 @@ onestep_MC <- function(Nsim, rho, N, T){
     
     results[i, ] <- GMM$one_step
   }
-  return(c(mean(results[, 1]), mean(results[, 2]), sd(results[, 1])))
+  return(c(mean(results[, 1]), mean(results[, 2]), sd(results[, 1]), sum(abs((results[, 1] - rho)/results[, 2]) > 1.96)/N_sim))
 }
-
 
 set.seed(11132)
 result_onestep_MC <- data.frame(rho = double(), T = integer(), N = integer(), 
                         result_rho = double(), result_ese = double(), 
-                        result_tse = double())
+                        result_tse = double(), size = double())
 
 for (rho in rho_par) {
   for (T in T_par) {
     for (N in N_par) {
       print(c(rho, T, N))
-      result <- onestep_MC(Nsim, rho, N, T)
+      result <- onestep_MC(N_sim, rho, N, T)
       
       result_onestep_MC <- rbind(result_onestep_MC, list(rho = rho, T = T, N = N, 
                                          result = result[1], result_ese = result[2], 
-                                         result_tse = result[3]))
+                                         result_tse = result[3], size = result[4]))
     }
   }
 }
 
 ### With two instruments ----
 
-twostep_MC <- function(Nsim, rho, N, T){
-  results <- matrix(data = 0, nrow = Nsim, ncol = 5)
+twostep_MC <- function(N_sim, rho, N, T){
+  results <- matrix(data = 0, nrow = N_sim, ncol = 5)
   colnames(results) <- c("coefs", "stdvs", "tstats", "pvals", "Jstat")
   
-  for (i in 1:Nsim){
+  for (i in 1:N_sim){
     df_GMM <- generate_ar1_panel(rho, N, T) %>% 
       group_by(individual) %>%
       mutate(yit = value) %>%
@@ -355,28 +349,27 @@ twostep_MC <- function(Nsim, rho, N, T){
     
     results[i, ] <- GMM$two_step
   }
-  return(c(mean(results[, 1]), mean(results[, 2]), sd(results[, 1])))
+  return(c(mean(results[, 1]), mean(results[, 2]), sd(results[, 1]), sum(abs((results[, 1] - rho)/results[, 2]) > 1.96)/N_sim))
 }
-
-y = df_GMM_IV2_y
-X = df_GMM_IV2_X
-Z = df_GMM_IV2_Z
 
 set.seed(11132)
 result_twostep_MC <- data.frame(rho = double(), T = integer(), N = integer(), 
                                 result_rho = double(), result_ese = double(), 
-                                result_tse = double())
+                                result_tse = double(), size = double())
 
 for (rho in rho_par) {
   for (T in T_par) {
     for (N in N_par) {
       print(c(rho, T, N))
-      result <- twostep_MC(Nsim, rho, N, T)
+      result <- twostep_MC(N_sim, rho, N, T)
       
       result_twostep_MC <- rbind(result_twostep_MC, list(rho = rho, T = T, N = N, 
                                                          result = result[1], result_ese = result[2], 
-                                                         result_tse = result[3]))
+                                                         result_tse = result[3], size = result[4]))
     }
   }
 }
 
+# Export results to Excel ----
+names <- list('result_FE_MC' = result_FE_MC, 'result_onestep_MC' = result_onestep_MC, 'result_twostep_MC' = result_twostep_MC)
+write.xlsx(names, file = 'Case assignment - Results.xlsx')
